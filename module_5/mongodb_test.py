9e1d2ba660b8247092e769ad4f6062fed5ceac5d
jbekter <88993240+jbekter@users.noreply.github.com>
2021-11-10 21:53:17 -0500

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,3 +1,11 @@
+""" 
+    Title: mongodb_test.py
+    Author: Josh Boettcher
+    Date: 10 November 2021
+    Description: Test program for connecting to a 
+                 MongoDB Atlas cluster
+"""
+
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.qcdpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
